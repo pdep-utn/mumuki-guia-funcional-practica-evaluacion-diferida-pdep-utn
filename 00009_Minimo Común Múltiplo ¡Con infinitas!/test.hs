ee938aa560efe6cdc8003e73169185ac0a269af5
lspigariol@gmail.com <lspigariol@gmail.com>
2022-05-06 14:17:36 +0000

    Mumuki Export on 2022-05-06 14:17:36 +0000

--- 00009_Minimo Común Múltiplo ¡Con infinitas!/test.hs
+++ 00009_Minimo Común Múltiplo ¡Con infinitas!/test.hs
@@ -3,5 +3,5 @@ describe "Minimo comun múltiplo" $ do
   it "mcm" $ do
     mcm 12 30 `shouldBe` 60
   
-  it "mcm" $ do
-    mcm 557 12 `shouldBe` 6684
+--  it "mcm" $ do
+--    mcm 557 12 `shouldBe` 6684
